feb3644b538b04a582acb3933a94885e41b605c8
LeonBentley <leonjbentley@gmail.com>
2017-06-30 16:15:20 +0100

    R code

--- accuracy_plots.R
+++ accuracy_plots.R
@@ -1,13 +1,24 @@
 require(ggplot2)
 
-k_accuracies <- read.delim("~/Documents/mash_assign/accuracies/k_accuracies.txt", header=FALSE)
+k_accuracies <- read.delim("~/Documents/accuracies/k_accuracies.txt", header=FALSE)
 colnames(k_accuracies) = c("accuracy", "n_folds")
 #ggplot(k_accuracies, aes(x=factor(n_folds), y=accuracy)) + geom_point() + geom_jitter(width = 0.1, height = 0.1)
 #ggplot(k_accuracies, aes(x=factor(n_folds), y=accuracy)) + geom_point() + geom_jitter(width = 0.1, height = 0.1)
 
-ggplot(k_accuracies, aes(x=factor(n_folds), y=accuracy)) + 
+ggplot(kmer_accuracies, aes(x=factor(n_folds), y=accuracy)) + 
   geom_boxplot() + geom_point() + 
   geom_jitter(width = 0.1, height = 0) +
   theme_bw(base_size=18) +
   xlab("N-folds cross-validation") +
+  ylim(0,1)
+
+
+kmer_accuracies <- read.delim("~/Documents/accuracies/kmer_accuracies.txt", header=FALSE)
+colnames(kmer_accuracies) = c("accuracy", "n_folds")
+
+ggplot(kmer_accuracies, aes(x=factor(n_folds), y=accuracy)) + 
+  geom_boxplot() + geom_point() + 
+  geom_jitter(width = 0.1, height = 0) +
+  theme_bw(base_size=18) +
+  xlab("K-mer Length") +
   ylim(0,1)
